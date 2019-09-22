e3106422506a6d8819a51c8a2224e614de636af5
Sebastian Bejm <125sbejm@gmail.com>
2019-09-22 13:54:23 -0700

    added more functions

--- Basic-AI/Basic-AI.cpp
+++ Basic-AI/Basic-AI.cpp
@@ -2,9 +2,61 @@
 //
 
 #include <iostream>
+#include <random>
 #include "Header.h"
 
 using namespace std;
+
+int checkDistance(char icon, char input[][MAX_SIZE]) {
+	int dist = 0;
+	return dist;
+}
+
+void updateField(char input[][MAX_SIZE], const int ROWS, const int COLS) {
+	for (int r = 0; r < ROWS; r++) {
+		for (int c = 0; c < COLS; c++) {
+			cout << input[r][c] << ' ';
+		}
+		cout << endl;
+	}
+}
+
+void generateTank(char input[][MAX_SIZE], const int ROWS, const int COLS) {
+	char tank = 'T';
+	
+	random_device rand;
+	mt19937 generator(rand());
+	uniform_int_distribution<> distr(1, ROWS - 2);
+	uniform_int_distribution<> distr2(1, COLS - 2);
+
+	int r = distr(generator);
+	int c = distr2(generator);
+
+	input[r][c] = 'T';
+
+	cout << endl;
+}
+
+void initRandomWalls(char input[][MAX_SIZE], const int ROWS, const int COLS) {
+
+	random_device rand;
+	mt19937 generator(rand());
+	uniform_int_distribution<> distr(1, ROWS-2);
+	uniform_int_distribution<> distr2(1, COLS - 2);
+
+	int r = distr(generator);
+	int c = distr2(generator);
+
+	// generate a wall somewhere in the middle of the field 
+
+	while (c < COLS-2) {
+		if (!(input[r][c] == 'T' && (input[r - 2][c - 2] != 'B' || input[r + 2][c - 2] != 'B'
+			|| input[r + 2][c + 2] != 'B' || input[r - 2][c + 2] != 'B'))) {
+			input[r][c] = 'W';
+		}
+		c++;
+	}
+}
 
 void initField(char input[][MAX_SIZE], const int ROWS, const int COLS) {
 	for (int r = 0; r < ROWS; r++) {
@@ -25,28 +77,21 @@ void initField(char input[][MAX_SIZE], const int ROWS, const int COLS) {
 			}
 		}
 	}
-
-	for (int r = 0; r < ROWS; r++) {
-		for (int c = 0; c < COLS; c++) {
-			cout << input[r][c] << ' ';
-		}
-		cout << endl;
-	}
 }
 
 int main() {
 	string input;
 	bool done = false;
 
-	int ROWS = 20;
-	int COLS = 40;
+	const int ROWS = 20;
+	const int COLS = 40;
 	char field[20][40];
 
-	char tank = 'T';
-
-
 	initField(field, ROWS, COLS);
+	generateTank(field, ROWS, COLS);
+	initRandomWalls(field, ROWS, COLS);
 
+	updateField(field, ROWS, COLS); // set up the field with tank and wall in place
 
 
 	return 0;
--- Basic-AI/Header.h
+++ Basic-AI/Header.h
@@ -2,8 +2,11 @@
 
 #define MAX_SIZE 40
 
-int checkDistance(char icon, int pos, int);
-bool hasCollided();
+int checkDistance(char icon, char input[][MAX_SIZE]);
+void move(char icon, int pos);
 
 void initField(char input[][MAX_SIZE], const int ROWS, const int COLS);
-char generateTank(char input[]);
+void initRandomWalls(char input[][MAX_SIZE], const int ROWS, const int COLS);
+void updateField(char input[][MAX_SIZE], const int ROWS, const int COLS);
+
+void generateTank(char input[][MAX_SIZE], const int ROWS, const int COLS);
