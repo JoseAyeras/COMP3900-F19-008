bbeabf5e943f183c19cef07cf29fcae30fa27560
Sebastian Bejm <125sbejm@gmail.com>
2019-09-30 15:00:41 -0700

    BFS stuff

--- Basic-AI/Basic-AI.cpp
+++ Basic-AI/Basic-AI.cpp
@@ -2,97 +2,38 @@
 //
 
 #include <iostream>
-#include <random>
 #include "Header.h"
+#include "BFS.h"
 
 using namespace std;
 
-int checkDistance(char icon, char input[][MAX_SIZE]) {
-	int dist = 0;
-	return dist;
-}
-
-void updateField(char input[][MAX_SIZE], const int ROWS, const int COLS) {
-	for (int r = 0; r < ROWS; r++) {
-		for (int c = 0; c < COLS; c++) {
-			cout << input[r][c] << ' ';
-		}
-		cout << endl;
-	}
-}
-
-void generateTank(char input[][MAX_SIZE], const int ROWS, const int COLS) {
-	char tank = 'T';
-	
-	random_device rand;
-	mt19937 generator(rand());
-	uniform_int_distribution<> distr(1, ROWS - 2);
-	uniform_int_distribution<> distr2(1, COLS - 2);
-
-	int r = distr(generator);
-	int c = distr2(generator);
-
-	input[r][c] = 'T';
-
-	cout << endl;
-}
-
-void initRandomWalls(char input[][MAX_SIZE], const int ROWS, const int COLS) {
-
-	random_device rand;
-	mt19937 generator(rand());
-	uniform_int_distribution<> distr(1, ROWS-2);
-	uniform_int_distribution<> distr2(1, COLS - 2);
-
-	int r = distr(generator);
-	int c = distr2(generator);
-
-	// generate a wall somewhere in the middle of the field 
-
-	while (c < COLS-2) {
-		if (!(input[r][c] == 'T' && (input[r - 2][c - 2] != 'B' || input[r + 2][c - 2] != 'B'
-			|| input[r + 2][c + 2] != 'B' || input[r - 2][c + 2] != 'B'))) {
-			input[r][c] = 'W';
-		}
-		c++;
-	}
-}
-
-void initField(char input[][MAX_SIZE], const int ROWS, const int COLS) {
-	for (int r = 0; r < ROWS; r++) {
-		if (r == 0 || r == ROWS - 1) {
-			for (int c = 0; c < COLS; c++) {
-				input[r][c] = 'B';
-
-			}
-		}
-		else {
-			for (int c = 0; c < COLS; c++) {
-				if (c == 0 || c == COLS - 1) {
-					input[r][c] = 'B';
-				}
-				else {
-					input[r][c] = '.';
-				}
-			}
-		}
-	}
-}
-
 int main() {
-	string input;
-	bool done = false;
+	char input = '0';
 
-	const int ROWS = 20;
-	const int COLS = 40;
-	char field[20][40];
+	const int ROWS = 9;
+	const int COLS = 10;
+	int field[ROWS][COLS] = {
+		{ 1, 0, 1, 1, 1, 0, 1, 1, 1, 1 },
+		{ 1, 0, 1, 0, 1, 0, 1, 0, 0, 1 },
+		{ 1, 1, 1, 0, 1, 0, 0, 1, 1, 1 },
+		{ 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 },
+		{ 1, 1, 1, 0, 1, 0, 0, 0, 1, 0 },
+		{ 1, 0, 1, 1, 1, 0, 0, 1, 0, 0 },
+		{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 1, 0, 1, 1, 1, 1, 0, 1, 1, 1 },
+		{ 1, 1, 1, 0, 0, 1, 1, 1, 0, 1 }
+	};
 
-	initField(field, ROWS, COLS);
 	generateTank(field, ROWS, COLS);
-	initRandomWalls(field, ROWS, COLS);
+	updateField(field, ROWS, COLS); // set up the field with tank and walls in place
+	cout << endl;
 
-	updateField(field, ROWS, COLS); // set up the field with tank and wall in place
+	Point src = { 0, 0 };
+	Point dest = { 8, 9 };
 
+	int dist = BFS(field, src, dest);
+	cout << "Distance by number of points taken: " << dist << endl;
+	cout << "Reached dest: " << dest.x << ", "<< dest.y << endl;
 
 	return 0;
 }
--- Basic-AI/Header.h
+++ Basic-AI/Header.h
@@ -1,12 +1,7 @@
 #pragma once
 
-#define MAX_SIZE 40
+#define MAX_SIZE 10
 
-int checkDistance(char icon, char input[][MAX_SIZE]);
-void move(char icon, int pos);
-
-void initField(char input[][MAX_SIZE], const int ROWS, const int COLS);
-void initRandomWalls(char input[][MAX_SIZE], const int ROWS, const int COLS);
-void updateField(char input[][MAX_SIZE], const int ROWS, const int COLS);
-
-void generateTank(char input[][MAX_SIZE], const int ROWS, const int COLS);
+void move(int input[][MAX_SIZE], int icon, int row, int col);
+void updateField(int input[][MAX_SIZE], const int ROWS, const int COLS);
+void generateTank(int input[][MAX_SIZE], const int ROWS, const int COLS);
