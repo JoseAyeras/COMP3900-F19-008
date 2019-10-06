210a8463d799a2249d55ebf76b467c74f4976713
Sebastian Bejm <125sbejm@gmail.com>
2019-10-05 18:35:02 -0700

    A-star algorithm stuff uploaded

---
+++ A-Star-AI/A-Star-AI.cpp
@@ -0,0 +1,33 @@
+// A-Star-AI.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <utility>
+#include "AStar.h"
+
+using namespace std;
+
+int main()
+{
+	int grid[ROW][COL] =
+	{
+		{ 1, 0, 1, 1, 1, 1, 0, 1, 1, 1 },
+		{ 1, 1, 1, 0, 1, 1, 1, 0, 1, 1 },
+		{ 1, 1, 1, 0, 1, 1, 0, 1, 0, 1 },
+		{ 0, 0, 1, 0, 1, 0, 0, 0, 0, 1 },
+		{ 1, 1, 1, 0, 1, 1, 1, 0, 1, 0 },
+		{ 1, 0, 1, 1, 1, 1, 0, 1, 0, 0 },
+		{ 1, 0, 0, 0, 0, 1, 0, 0, 0, 1 },
+		{ 1, 0, 1, 1, 1, 1, 0, 1, 1, 1 },
+		{ 1, 1, 1, 0, 0, 0, 1, 0, 0, 1 }
+	};
+
+	updateGrid(grid);
+
+	Pair src = make_pair(0, 0);
+	Pair dest = make_pair(8, 0);
+	aStarSearch(grid, src, dest);
+
+	return 0;
+}
+
